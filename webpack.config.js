f7aaba5d77ab095d522cebc3cc2e28805119c5cd
Abhinav Rai <raiabhinav.rai1994@gmail.com>
2021-01-03 14:03:31 +0530

    gh pages build deploy

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ if (!devMode) {
 module.exports = {
     entry: { index: path.resolve(__dirname, 'src', 'index.js') },
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'build'),
         filename: '[name].[hash].js',
     },
     module: {
